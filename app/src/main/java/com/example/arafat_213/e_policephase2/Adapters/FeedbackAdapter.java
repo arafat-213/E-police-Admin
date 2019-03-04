a07c3202fb674ce41e2c77ae337a08c29dd6dc1c
arafat-213 <tai.arafat.at@gmail.com>
2019-03-04 19:38:43 +0530

    somehow created a huge mess

---
+++ app/src/main/java/com/example/arafat_213/e_policephase2/Adapters/FeedbackAdapter.java
@@ -0,0 +1,59 @@
+package com.example.arafat_213.e_policephase2.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.arafat_213.e_policephase2.Models.Feedback;
+import com.example.arafat_213.e_policephase2.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class FeedbackAdapter extends FirebaseRecyclerAdapter<Feedback, FeedbackAdapter.FeedbackViewHolder> {
+
+
+    private static final String TAG = "FeedbackAdapter";
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public FeedbackAdapter(@NonNull FirebaseRecyclerOptions<Feedback> options) {
+        super(options);
+    }
+
+
+    @NonNull
+    @Override
+    public FeedbackViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.item_feedback, viewGroup, false);
+        return new FeedbackViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeedbackViewHolder feedbackViewHolder, int position, @NonNull Feedback model) {
+//        feedbackViewHolder.policeStationTV.setText(model.getPoliceStation());
+        feedbackViewHolder.policeOfficerNameTV.setText(model.getPoliceOfficer());
+        feedbackViewHolder.policeRatingTV.setText(" " + model.getRating() + " ★");
+        feedbackViewHolder.descriptionTV.setText(model.getDescription());
+    }
+
+    public class FeedbackViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView policeStationTV, policeOfficerNameTV, policeRatingTV, descriptionTV;
+
+        public FeedbackViewHolder(@NonNull View itemView) {
+            super(itemView);
+//            policeStationTV = itemView.findViewById(R.id.feedbackStationTV);
+            policeOfficerNameTV = itemView.findViewById(R.id.complaintNameTV);
+            policeRatingTV = itemView.findViewById(R.id.feedbackRatingTV);
+            descriptionTV = itemView.findViewById(R.id.complaintContentET);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/arafat_213/e_policephase2/Models/Feedback.java
@@ -0,0 +1,85 @@
+package com.example.arafat_213.e_policephase2.Models;
+
+public class Feedback {
+    private String policeStation;
+    private String policeOfficer;
+    private String rating;
+    private String description;
+    private String userName;
+    private String policemanID;
+
+
+    public Feedback(String policeStation, String policeOfficer, String rating, String description, String userName, String policemanID) {
+        this.policeStation = policeStation;
+        this.policeOfficer = policeOfficer;
+        this.rating = rating;
+        this.description = description;
+        this.userName = userName;
+        this.policemanID = policemanID;
+    }
+
+    public Feedback() {
+    }
+
+
+    public String getPoliceStation() {
+        return policeStation;
+    }
+
+    public void setPoliceStation(String policeStation) {
+        this.policeStation = policeStation;
+    }
+
+    public String getPoliceOfficer() {
+        return policeOfficer;
+    }
+
+    public void setPoliceOfficer(String policeOfficer) {
+        this.policeOfficer = policeOfficer;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPolicemanID() {
+        return policemanID;
+    }
+
+    public void setPolicemanID(String policemanID) {
+        this.policemanID = policemanID;
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "policeStation='" + policeStation + '\'' +
+                ", policeOfficer='" + policeOfficer + '\'' +
+                ", rating='" + rating + '\'' +
+                ", description='" + description + '\'' +
+                ", userName='" + userName + '\'' +
+                ", policemanID='" + policemanID + '\'' +
+                '}';
+    }
+
+}
